20e9ebf4d5cdde6acf0925ff1ada57537d3856b6
Alymcc <am12th@brocku.ca>
2025-11-06 13:45:41 -0500

    Update with actual assets

--- src/main/java/peekaboo/GameLauncher.java
+++ src/main/java/peekaboo/GameLauncher.java
@@ -5,12 +5,16 @@ import de.gurkenlabs.litiengine.resources.Resources;
 import de.gurkenlabs.litiengine.sound.Sound;
 
 public class GameLauncher {
-  public static void launch() {
-    String mapPath = "maps/Temp_img.PNG";
-    String soundPath = "audio/ES_6oth_Christmas_-_par.wav";
+    public static void main(String[] args) {
+        launch();
+    }
 
-    Game.world().loadEnvironment(mapPath);
-    Sound ambient = Resources.sounds().get(soundPath);
-    Game.audio().playMusic(ambient);
-  }
+    public static void launch() {
+        String mapPath = "Assets/maps/Map_Party_Room.png";
+        String soundPath = "Assets/audio/Menu Music.mp3";
+
+        Game.world().loadEnvironment(mapPath);
+        Sound ambient = Resources.sounds().get(soundPath);
+        Game.audio().playMusic(ambient);
+    }
 }
