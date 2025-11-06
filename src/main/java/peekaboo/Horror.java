ae2a5b78dd55224ecbbe7fcf2e56d0483a7bd939
Alymcc <am12th@brocku.ca>
2025-11-05 20:09:53 -0500

    Attempt 2
    Please work and run the files!!!

--- src/main/java/peekaboo/Horror.java
+++ src/main/java/peekaboo/Horror.java
@@ -3,7 +3,8 @@ package peekaboo;
 import de.gurkenlabs.litiengine.Game;
 
 public class Horror {
-    public static void main(String[] args) {
-        Game.start();
-    }
+  public static void main(String[] args) {
+    Game.init();   // Initializes the game engine
+    Game.start();  // Starts the game loop
+  }
 }
