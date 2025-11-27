0d3a64a0194b2b04c5eab3b45979067d5656770d
Alymcc <am12th@brocku.ca>
2025-11-27 14:17:26 -0500

    Starting with Godot

--- litiengine/src/main/java/de/gurkenlabs/litiengine/abilities/targeting/EnemyTargetingStrategy.java
+++
@@ -1,8 +0,0 @@
-package de.gurkenlabs.litiengine.abilities.targeting;
-
-public class EnemyTargetingStrategy extends OtherEntityTargetingStrategy {
-
-  public EnemyTargetingStrategy(boolean multiTarget, boolean sortByDistance, boolean includeDead) {
-    super(multiTarget, sortByDistance, false, false, includeDead);
-  }
-}
--- litiengine/src/main/java/de/gurkenlabs/litiengine/abilities/targeting/FriendlyTargetingStrategy.java
+++
@@ -1,8 +0,0 @@
-package de.gurkenlabs.litiengine.abilities.targeting;
-
-public class FriendlyTargetingStrategy extends OtherEntityTargetingStrategy {
-
-  public FriendlyTargetingStrategy(boolean multiTarget, boolean sortByDistance, boolean includeDead) {
-    super(multiTarget, sortByDistance, true, false, includeDead);
-  }
-}
