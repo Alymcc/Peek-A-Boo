b4827ee388530699efdc7ec73d7adcb51b65f7fc
Alymcc <am12th@brocku.ca>
2025-12-02 14:12:00 -0500

    New engine

---
+++ new-game-project/resources/sprites/furniture/Furniture.tsx
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.2" name="Furniture" tilewidth="89" tileheight="68" tilecount="15" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <properties>
+   <property name="collsion" type="bool" value="false"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="mat"/>
+  </properties>
+  <image source="bathroom_mat.png" width="62" height="24"/>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collsion" type="bool" value="false"/>
+   <property name="interactable" type="bool" value="true"/>
+   <property name="type" value="Computer"/>
+  </properties>
+  <image source="computer.png" width="26" height="16"/>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collsion" type="bool" value="true"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Couch"/>
+  </properties>
+  <image source="couch.png" width="46" height="35"/>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collsion" type="bool" value="false"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Mat"/>
+  </properties>
+  <image source="couch_mat.png" width="80" height="60"/>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collsion" type="bool" value="false"/>
+   <property name="interactable" type="bool" value="true"/>
+   <property name="type" value="Cupcake"/>
+  </properties>
+  <image source="cupcakes.png" width="24" height="15"/>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collsion" type="bool" value="true"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Desk"/>
+  </properties>
+  <image source="desk.png" width="89" height="68"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collsion" type="bool" value="false"/>
+   <property name="interactable" type="bool" value="true"/>
+   <property name="type" value="File"/>
+  </properties>
+  <image source="files.png" width="13" height="11"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="collsion" type="bool" value="true"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Lamp"/>
+  </properties>
+  <image source="lamp.png" width="14" height="20"/>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collsion" type="bool" value="false"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Mat"/>
+  </properties>
+  <image source="office_mat.png" width="61" height="21"/>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="collsion" type="bool" value="false"/>
+   <property name="interactable" type="bool" value="true"/>
+   <property name="type" value="Phone"/>
+  </properties>
+  <image source="phone.png" width="19" height="17"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="collsion" type="bool" value="true"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Plant"/>
+  </properties>
+  <image source="plant.png" width="13" height="30"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="collsion" type="bool" value="true"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Sink"/>
+  </properties>
+  <image source="sink.png" width="26" height="32"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collsion" type="bool" value="true"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Table"/>
+  </properties>
+  <image source="table.png" width="21" height="22"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collsion" type="bool" value="true"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Toilet"/>
+  </properties>
+  <image source="toilet.png" width="21" height="29"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="collsion" type="bool" value="false"/>
+   <property name="interactable" type="bool" value="false"/>
+   <property name="type" value="Toilet Paper"/>
+  </properties>
+  <image source="toiletpaper.png" width="14" height="11"/>
+ </tile>
+</tileset>
